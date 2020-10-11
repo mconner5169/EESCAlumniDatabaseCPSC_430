945117b4284c561e081c2e35492e07f65156b177
Pratima <pratimakandel40@gmail.com>
2020-10-11 10:43:17 -0400

    set up environment

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3000
+
+//Load .env file
+require('dotenv').config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use(express.static("public"))
+
+const uri = process.env.ATLAS_URI
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log("Connection to MongoDB successful")
+})
+
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.listen(port,
+    () => console.log(`Server is running on port ${port}`))

dad89b23183eecbf326703c79f178e66f2f33116
Pratima <pratimakandel40@gmail.com>
2020-11-04 18:09:53 -0500

    Removed not used dependencies

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000
 require('dotenv').config();
 
 app.use(cors());
-//app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

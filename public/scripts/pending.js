6a3c2eb2d413c69580c5ddb78e3121cc4ecf14ce
Daniel Glista <dnglista@gmail.com>
2020-11-11 10:33:18 -0500

    pending dashboard

--- public/scripts/pending.js
+++ public/scripts/pending.js
@@ -1,22 +1,15 @@
 
 // EVENT LISTENERS
 
-//  tbody event listener for displaying update and delete buttons
+//  tbody event listener for displaying accept and reject buttons
 document.querySelector('tbody').addEventListener('mouseover', buttonVisibility);
 
-// table event listener for hiding all update and delete buttons when mouse leaves table
+// table event listener for hiding all accept and reject buttons when mouse leaves table
 document.querySelector('table').addEventListener('mouseleave', (event) => {
     document.querySelectorAll('tbody button').forEach((button) => {
         button.style.visibility = 'hidden';
     });
 });
-
-// from event listener for dynamically setting form POST request url 
-document.querySelector('.alumni_form').addEventListener('submit', (event) => {
-    event.preventDefault();
-    POST_alumni_form(document.querySelector('#submit').getAttribute('crud_type') == 'add' ? '/admin/create' : '/admin/' + document.querySelector('form').id + '/update')
-});
-
 
 function buttonVisibility(event) {
     document.querySelectorAll('tbody button').forEach((button) => {
@@ -39,79 +32,24 @@ function buttonVisibility(event) {
     }
 }
 
-function addDeleteEventListeners() {
-    document.querySelectorAll('.delete_btn').forEach((btn) => { 
+function addButtonEventListeners() {
+    document.querySelectorAll('.accept_btn').forEach((btn) => { 
+        btn.addEventListener('click', POST_approve_alumni);
+    })
+    document.querySelectorAll('.reject_btn').forEach((btn) => { 
         btn.addEventListener('click', DELETE_alumni);
     })
 }
 
-addDeleteEventListeners();
+addButtonEventListeners();
+
 
 // PAGE RENDERING FUNCTIONS
-
-// Modal handler
-$('#form_modal').on('show.bs.modal', function (event) {
-    let errorList = document.querySelector('#errorList');
-    errorList.innerHTML = '';
-    let button = $(event.relatedTarget); 
-    let crud_type = button.data('type'); 
-    let modal = $(this);
-    modal.find('.modal-title').text(crud_type + ' Alumni Entry');
-
-    if (crud_type == 'Update') {
-        GET_alumni('/api/alumni/' + button[0].getAttribute('alumni_id'), (alumni) => {
-            document.querySelector('#firstName').value = alumni.firstName;
-            document.querySelector('#lastName').value = alumni.lastName;
-            document.querySelector('#email').value = alumni.email;
-            document.querySelector('#gradYear').value = alumni.gradYear;
-            document.querySelector('#degreeType').value = alumni.degreeType;
-            document.querySelector('#occupation').value = alumni.occupation;
-            document.querySelector('#description').value = alumni.description === undefined ? '' : alumni.description;
-            document.querySelector('#emailList').checked = alumni.emailList;
-
-            document.querySelector('form').id = button[0].getAttribute('alumni_id');
-
-
-        });
-    } else {
-        document.querySelector('form').id = '';
-    }
-
-    document.querySelector('#submit').setAttribute('crud_type', crud_type.toLowerCase());
-});
-
-// Renders modal with errors
-function renderFormErrors(errors) {
-    errors = JSON.parse(errors);
-    let modal = $('#form_modal')
-    let errorList = document.querySelector('#errorList');
-    errorList.innerHTML = '';
-    for (i in errors) {
-        let errorMsg = document.createTextNode('*' + errors[i].msg)
-        let br = document.createElement('br')
-        errorList.appendChild(errorMsg);
-        errorList.appendChild(br);
-    }
-
-}
-
-// Remove user inputed values in form
-function resetForm() {
-    document.querySelector('#firstName').value = '';
-    document.querySelector('#lastName').value = '';
-    document.querySelector('#gradYear').value = '';
-    document.querySelector('#degreeType').value = '';
-    document.querySelector('#occupation').value = '';
-    document.querySelector('#email').value = '';
-    document.querySelector('#emailList').value = '';
-    document.querySelector('#description').value = '';
-
-}
 
 
 // Renders table with updated database
 function renderTable() {
-    GET_alumni_entries((alumnis) => {
+    GET_pending_alumni_entries((alumnis) => {
         alumnis.sort(function(a, b) {let textA = a.lastName.toUpperCase(); let textB = b.lastName.toUpperCase(); return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;});
         let tbody = document.querySelector('tbody');
         let clone = tbody.cloneNode(false);
@@ -128,12 +66,12 @@ function renderTable() {
             <td class='text-truncate'>${alumnis[i].occupation}</td>
             <td class='text-truncate'>${alumnis[i].email}</td>
             <td>${alumnis[i].emailList}</td>
-            <td class='px-0'><button class='btn btn-secondary btn-sm mr-3' data-toggle='modal' data-target='#form_modal' data-type='Update' alumni_id='${alumnis[i]._id}'>Update</button></td>
-            <td class='px-0'><button class='btn btn-danger btn-sm delete_btn' alumni_id='${alumnis[i]._id}'>Delete</button></td>`;
+            <td class='px-0'><button class='btn btn-success btn-sm mr-3 accept_btn' alumni_id='${alumnis[i]._id}'>Accept</button></td>
+            <td class='px-0'><button class='btn btn-danger btn-sm reject_btn' alumni_id='${alumnis[i]._id}'>Reject</button></td>`;
             tbody.appendChild(tr);
         }
 
-        addDeleteEventListeners();
+        addButtonEventListeners();
     })
 }
 
@@ -180,9 +118,9 @@ function POST_alumni_form(url) {
 }
 
 // Alumni entries get handler using ajax
-function GET_alumni_entries(callback) {
+function GET_pending_alumni_entries(callback) {
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', '/api/alumnis', true);
+    xhr.open('GET', '/api/alumnis/pending', true);
     
     xhr.send();
 
@@ -192,23 +130,6 @@ function GET_alumni_entries(callback) {
 
     xhr.onerror = function() {
         console.log('XMLHTTPRequest error');
-    }
-
-}
-
-// Returns a singel alumni entry
-function GET_alumni(url, callback) {
-
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.send();
-
-    xhr.onload = () => {
-        if (callback) {callback(JSON.parse(xhr.responseText))}
-    }
-
-    xhr.onerror = () => {
-        console.log('XMLHTTMLRequest Error')
     }
 
 }
@@ -223,8 +144,6 @@ function DELETE_alumni(event) {
     xhr.onload = () => {
         if (xhr.status == 200) {
             renderTable();
-            resetForm();
-            $('#form_modal').modal('hide');
         }
     }
 
@@ -233,4 +152,22 @@ function DELETE_alumni(event) {
     }
 
 
+}
+
+function POST_approve_alumni(event) {
+    let url = '/admin/' + event.srcElement.getAttribute('alumni_id') + '/approve';
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.send();
+
+    xhr.onload = () => {
+        if (xhr.status == 200) {
+            renderTable();
+        }
+    }
+
+    xhr.onerror = () => {
+        console.log('XMLHTTMLRequest Error')
+    }
 }

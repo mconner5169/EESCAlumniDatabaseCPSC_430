788be9a38822504dd107188252705471a7e860d3
Daniel Glista <dnglista@gmail.com>
2020-12-08 15:03:18 -0500

    updated alumni ui

--- public/scripts/status.js
+++ public/scripts/status.js
@@ -25,6 +25,10 @@ function renderAlumni() {
                 } else {
                 expand(alumni);     
                 }           
+            } else {
+                // document.querySelector('.card').classList.toggle('expanded', true);
+                // document.querySelector('#error-msg').innerHTML = 'No entries found. This email is either incorrect or the entry has been deleted.';
+                // document.querySelector('.card').style.height = '11rem';
             }
         })
     }
@@ -40,7 +44,13 @@ function expand(alumni) {
     document.querySelector('#description').value = alumni.description;
     document.querySelector('#emailList').checked = alumni.emailList;
     document.querySelector('#emailList').setAttribute('disabled', true);
-    //alumni.emailList == 'on' ? 'yes' : 'no';
+    document.querySelector('#status').innerHTML = alumni.status.toUpperCase();
+
+    if (alumni.status == 'approved') {
+        document.querySelector('#status').classList.add('text-success');
+    } else if (alumni.status == 'pending') {
+        document.querySelector('#status').classList.add('text-warning');
+    }
 
     if (window.innerWidth <= 768) {
         document.querySelector('.card').style.width = '100%';
@@ -49,7 +59,7 @@ function expand(alumni) {
         document.querySelector('#email-search').classList.toggle('col-md-10', true);
         document.querySelector('#search-btn-container').classList.toggle('col-md-2', true);
     }
-    document.querySelector('.card').style.height = '45rem';
+    document.querySelector('.card').style.height = '36rem';
     setTimeout( () => {
         document.querySelector('#alumni_entry').style.display = 'block';
         document.querySelector('.card').classList.toggle('expanded', true);
